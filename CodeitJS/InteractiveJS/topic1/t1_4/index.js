12e9b91d57854c800741f1d541793009c1c2a044
KimMyeongSu <myeongsu@likelion.org>
2021-05-16 13:32:28 +0900

    Add CodeJS interactiveJS topic1 t1_4

---
+++ CodeitJS/InteractiveJS/topic1/t1_4/index.js
@@ -0,0 +1,28 @@
+/** 
+ * [마우스 버튼 이벤트]
+ * 
+ * > MouseEvent.button
+ * 0: 마우스 왼쪽 버튼
+ * 1: 마우스 휠
+ * 2: 마우스 오른쪽 버튼
+ * 
+ * > MouseEvent.type
+ * click: 마우스 왼쪽 버튼을 눌렀을 때
+ * contextmenu: 마우스 오른쪽 버튼을 눌렀을 때
+ * dblclick: 동일한 위치에서 빠르게 두번 click할 때
+ * mousedown: 마우스 버튼을 누른 순간
+ * mouseup: 마우스 버튼을 눌렀다 뗀 순간
+ */
+
+/* mouseover: 마우스 포인터가 요소 밖에서 안으로 이동할 때
+ * mouseout: 마우스 포인터가 요소 안에서 밖으로 이동할 때 
+ *
+ * > MouseEvent.clientX, clientY
+ * : 화면에 표시되는 창 기준 마우스 포인터 위치   
+ * 
+ * > MouseEvent.pageX, pageY
+ * : 웹 문서 전체 기준 마우스 포인터 위치
+ * 
+ * > MouseEvent.offsetX, offsetY
+ * : 이벤트가 발생한 요소 기준 마우스 포인터 위치
+ */
