7dcf566850775c0753ba0ac56cc80c806c5703d7
KimMyeongSu <myeongsu@likelion.org>
2021-06-08 15:13:50 +0900

    CodeJS 2번째 REST API

--- CodeitJS/InteractiveJS/topic4/topic4_2/index.js
+++ CodeitJS/InteractiveJS/topic4/topic4_2/index.js
@@ -1,3 +1,25 @@
-    fetch('https://jsonplaceholder.typicode.com/users')
+    fetch('데이터주소')
     .then((response)=> response.text())
-    .then((result) => {console.log(result)})
+    .then((result) => {
+        const divApp = document.querySelector('#app')
+        divApp.innerHTML = result;
+    })
+
+
+
+    const newMember = {
+        name: 'jerry',
+        email: 'jerry@codeitmall.kr',
+        department: 'engineering',
+    };
+
+
+    fetch('데이터주소',{
+        method: 'POST',
+        body: JSON.stringify(newMember),
+    })
+    .then((response)=> response.text())
+    .then((reslut)=> {
+        const divApp = document.querySelector('#app')
+        divApp.innerHTML = result;
+    })
